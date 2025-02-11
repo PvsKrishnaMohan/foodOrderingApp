9d9081f199bf394bf2a8ffa36aef8a4a641e7913
PvsKrishnaMohan <pvskrishnamohan479@gmail.com>
2025-02-11 23:56:59 +0530

    testing

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,6 +6,7 @@ import UserClassCard from './UserClassCard'
 const About = () => {
   return (
     <div>
+      <h1 className='text-2xl font-bold'>About</h1>
       <UserCard name={"Krishna Mohan"} location = "AndhraPradesh" gender="male"/>
       <UserClassCard name={"Krishna Mohan"} location = "AndhraPradesh" gender="male"/>
     </div>
